416067a246475a1f1196174086d58f269aa768c4
Eren Siegman <eren.siegman@esiegman.dev>
2025-10-23 22:27:39 -0400

    test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
-mopule.exports = {
+export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
