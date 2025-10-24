517075f8088004423288beca53a915c508df0c19
Eren Siegman <eren.siegman@esiegman.dev>
2025-10-23 21:13:55 -0400

    Test

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -2,10 +2,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: false,
+  projectId: "j1xlwjt0",
+  dataset: "production",
+  useCdn: true,
   apiVersion: "2023-05-03",
-  // perspective: 'published' // Use 'published' for server-side fetching
-  // token: import.meta.env.SANITY_API_READ_TOKEN, // Use if you added a token
 });
